0b6e47e5aa90de00b9721a8930590d91262f3914
Mylie Walker <myliewalker@Mylies-MacBook-Pro.local>
2019-02-05 16:42:02 -0500

    correct

--- recurse.s
+++ recurse.s
@@ -6,62 +6,53 @@ main:
 
     li $v0, 5
     syscall
-    
     move $a0, $v0
+
+    move $t2, $a0
 
 f:
     addi $sp, $sp, -8
     sw $ra, 4($sp)
     sw $s0, 0($sp)
 
-    #base case
     beqz $a0, base
 
     add $s0, $a0, $0
-    li $t1, 1
-    sub $a0, $a0, $t1
+    addi $a0, $a0, -1
+
     jal f
 
     li $t0, 4
     mul $s1, $s0, $t0 
     li $t1, 2
     mul $s2, $v0, $t1
-    add $s3, $s1, $s2
-    addi $v0, $s3, 3 
+    add $v0, $s1, $s2
+    addi $v0, $v0, 3 
 
-    move $a2, $v0
+    addi $t2, $t2, -1
+    bnez $t2, clean
 
-    # move $a0, $v0
-    # li $v0, 1
-    # syscall
+    move $v1, $v0
 
-    # la $a0, nln
-    # li $v0, 4
-    # syscall
+    move $a0, $v0
+    li $v0, 1
+    syscall
+
+    la $a0, nln
+    li $v0, 4
+    syscall
+
+    move $v0, $v1
+    j clean
 
 base:
     li $v0, 5
-    move $a1, $a0
-    li $a0, 5
     j clean
 
 clean:
     lw $s0, 0($sp)
     lw $ra, 4($sp)
     addi $sp, $sp, 8
-
-    beqz $a1, print
-
-    jr $ra
-
-print:
-    li $v0, 1
-    move $a0, $a2
-    syscall
-
-    li $v0, 4
-    la $a0, nln
-    syscall
 
     jr $ra
 
