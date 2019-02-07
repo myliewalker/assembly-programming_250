9101187767d090c638a943247d2937726938015e
Mylie Walker <myliewalker@Mylies-MacBook-Pro.local>
2019-02-07 17:47:24 -0500

    correct unordered

--- HoopsStats.s
+++ HoopsStats.s
@@ -1,26 +1,29 @@
-#t6 stores list
+#t6 stores list - address is $t6
 #a2 stores counter
 .text
 main:
+    #Initialize counter
     li $a2, 0
 
-new:
-    li $v0, 9 #create space for a new player
+allocate:
+    #Allocate memory
+    li $v0, 9
     li $a0, 112
     syscall
     move $t6, $v0
 
 name:
+    #Read name
     li $v0, 4
     la $a0, name_prompt
     syscall
 
     li $v0, 8
-    la $a0, t_name
+    la $a0, t_name #load name into a0
     li $a1, 64
     syscall
 
-    la $t0, t_name
+    la $t0, t_name #load name into t0
     li $t2, 10
 
 nameLoop:
@@ -40,7 +43,7 @@ compare:
     lb $t3, 0($t0)
     lb $s3, 0($s0)
 
-    bne $t3, $s3, addName
+    bne $t3, $s3, loadName
 
     li $s2, 0
     seq $t4, $t3, $s2
@@ -53,10 +56,25 @@ compare:
     addi $s0, $s0, 1
     j compare
 
-addName:
+loadName:
     #Store name
-    la $t0, t_name
-    sw $t0, 0($t6)
+    la $s0, t_name
+
+    li $t1, 0
+
+addName:
+    lb $t0, 0($s0)
+    sb $t0, 0($t6)
+    beq $t0, $0, inc
+
+    addi $t1, $t1, 1
+    addi $s0, $s0, 1
+    addi $t6, $t6, 1
+
+    j addName
+
+inc:
+    sub $t6, $t6, $t1
 
 number:
     #Read player stats
@@ -90,7 +108,7 @@ next:
     sw $0, 80($t6)
     addi $a2, $a2, 1
 
-    j new
+    j allocate
 
 # sort:
 #     j print
@@ -100,8 +118,8 @@ print:
     addi $t6, $t6, -112
     beqz $a2, exit
 
-    li $v0, 4 #ISSUE: printing done instead of name - create a new name
-    lw $a0, 0($t6)
+    li $v0, 4
+    move $a0, $t6
     syscall
 
     li $v0, 4
@@ -138,6 +156,7 @@ exit:
     year_prompt: .asciiz "Enter the player's year:\n"
     
     t_name: .space 64
+    p_name: .space 64
     done: .asciiz "DONE\n"
     equal: .asciiz "Entered done \n"
     nln: .asciiz "\n"
