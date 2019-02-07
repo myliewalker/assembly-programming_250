e63dec2f9e5a88b83d13e220d02d98daefc8729e
Mylie Walker <myliewalker@Mylies-MacBook-Pro.local>
2019-02-07 12:28:01 -0500

    prints one player

--- HoopsStats.s
+++ HoopsStats.s
@@ -1,4 +1,15 @@
+#t6 stores list
+#t7 stores counter
+.text
 main:
+    li $v0, 9 #create space for a new player
+    li $a0, 112
+    syscall
+    move $t6, $v0
+
+    li $t7, 0
+
+name:
     li $v0, 4
     la $a0, name_prompt
     syscall
@@ -11,14 +22,14 @@ main:
     la $t0, t_name
     li $t2, 10
 
-readName:
+nameLoop:
     #Read player name
     lb $t1, 0($t0)
-    beq $t1, $t2, markDone
+    beq $t1, $t2, remove
     addi $t0, $t0, 1
-    j readName
+    j nameLoop
 
-markDone:
+remove:
     sb $0, 0($t0)
     la $t0, t_name
     la $s0, done
@@ -28,96 +39,98 @@ compare:
     lb $t3, 0($t0)
     lb $s3, 0($s0)
 
-    bne $t3, $s3, readStats
+    bne $t3, $s3, addName
 
     li $s2, 0
     seq $t4, $t3, $s2
     seq $s4, $s3, $t2
     seq $t5, $t4, $s4
     li $s5, 1
-    beq $t5, $s5, sort
+    beq $t5, $s5, print
 
     addi $t0, $t0, 1
     addi $s0, $s0, 1
     j compare
 
-readStats:
+addName:
+    #Store name
+    la $t0, t_name
+    sw $t0, 0($t6)
+
+number:
     #Read player stats
-    li $v0, 4               #Get number
+    li $v0, 4               
     la $a0, number_prompt
     syscall
+
     li $v0, 5
     syscall
-    move $s0, $v0
+    sw $v0, 64($t6)
 
-    li $v0, 4               #Get points
+points:
+    li $v0, 4               
     la $a0, points_prompt
     syscall
-    li $v0, 6
-    syscall
 
-    li $v0, 4               #Get year
+    li $v0, 6
+    syscall  
+    s.s $f0, 68($t6)
+
+year:
+    li $v0, 4               
     la $a0, year_prompt
     syscall
+
     li $v0, 5
     syscall
-    move $s1, $v0
+    sw $v0, 76($t6)
 
-    # j print
+next:
+    sw $0, 80($t6)
+    addi $t7, $t7, 1
 
-# build:
-#     li $v0, 9 #create space for a new player
-#     li $a0, 80
-#     syscall
+    j main
 
-#     la $t0, t_name
-#     lw $t0, 0($t3) #name
-#     lw $s0, 64($t3) #number
-#     lw $s1, 68($t3) #points
-#     lw $s2, 76($t3) #year
+# sort:
+#     j print
 
-    #ADD 32 bits for the next 
-    #1. malloc space for player
-    #2. copy player data
-    #3. add to players
-    
 print:
+    beqz $t7, exit
+
     li $v0, 4
-    la $a0, t_name
+    lw $a0, 0($t6)
     syscall
+
     li $v0, 4
-    la $a0, nln
+    la $a0, space
     syscall
 
     li $v0, 1
-    move $a0, $s0
-    syscall
-    li $v0, 4
-    la $a0, nln
+    lw $a0, 64($t6)
     syscall
 
-    li $v0, 2
-    mov.s $f12, $f0
-    syscall
     li $v0, 4
-    la $a0, nln
+    la $a0, space
     syscall
+
+    # li $v0, 2
+    # lw $f12, 64($t6)
+    # syscall
 
     li $v0, 1
-    move $a0, $s1
+    lw $a0, 76($t6)
     syscall
+
     li $v0, 4
     la $a0, nln
     syscall
-    jr $ra
 
-sort:
-    li $v0, 4
-    la $a0, equal
-    syscall
-    jr $ra
+    addi $t7, $t7, -1
+    addi $t6, $t6, -112
+    j print
 
-clean:
+exit:
+    jr $ra
 
 .data
 .align 2
@@ -125,17 +138,9 @@ clean:
     number_prompt: .asciiz "Enter the player's number:\n"
     points_prompt: .asciiz "Enter the player's average points per game: \n"
     year_prompt: .asciiz "Enter the player's year:\n"
+    
     t_name: .space 64
-
-    # bb_player: 
-    #     bb_player.name  .space 64
-    #     bb_player.number .space 4
-    #     bb_player.points .space 8
-    #     bb_player.year .space 4
-        # .float 
-        # .integer
-
     done: .asciiz "DONE\n"
-
     equal: .asciiz "Entered done \n"
     nln: .asciiz "\n"
+    space: .asciiz " "
